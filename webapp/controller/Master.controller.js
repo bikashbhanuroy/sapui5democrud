55bc6e4a52498141b46f03b7469eca25602cdebd
Bikash Bhanu Roy <sapui5trainingdemo@gmail.com>
2023-08-24 07:36:47 +0000

    latest changes as on 24-Aug-23

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -13,19 +13,19 @@ sap.ui.define([
         return Controller.extend("nw.sapui5democrud.controller.Master", {
             onInit: function () {
                 var that = this;
-                var odataModel = new ODataModel("/V2/(S(4v03bfpjrejawmqbuhnu2ykc))/OData/OData.svc");
-                odataModel.read("/Products", {
-                    success: function (oData, oResponse) {
-                        MessageBox.success("Success");
+                // var odataModel = new ODataModel("/V2/(S(4v03bfpjrejawmqbuhnu2ykc))/OData/OData.svc");
+                // odataModel.read("/Products", {
+                //     success: function (oData, oResponse) {
+                //         MessageBox.success("Success");
 
-                    },
-                    error: function (oError) {
-                        MessageBox.error("Error");
-                    }
-                });
-                this.getView().setModel(odataModel,"nwModel");
+                //     },
+                //     error: function (oError) {
+                //         MessageBox.error("Error");
+                //     }
+                // });
+                // this.getView().setModel(odataModel,"nwModel");
             },
-            updateData: function(){
+            updateData: function () {
                 var list = this.getView().byId("list");
                 var selItem = list.getSelectedItem();
                 var title = selItem.getTitle();
@@ -37,77 +37,77 @@ sap.ui.define([
                 };
 
                 var path = "/Categories(" + title + ")";
-                var odataModel = this.getView().getModel("nwModel");
+                var odataModel = this.getOwnerComponent().getModel();
                 // @ts-ignore
-                odataModel.update(path,payload,{
-                    headers:{
-                        "Content-ID":"1"
+                odataModel.update(path, payload, {
+                    headers: {
+                        "Content-ID": "1"
                     },
-                    success: function(data,response){
+                    success: function (data, response) {
                         MessageBox.success("Successfully Updated");
                     },
-                    error: function(error){
+                    error: function (error) {
                         MessageBox.error("Error while updating the data");
                     }
                 });
             },
-            createData: function(){
+            createData: function () {
                 var ID = this.getView().byId("idinput").getValue();
                 var Name = this.getView().byId("nameinput").getValue();
                 var data = {
                     ID: parseInt(ID),
                     Name: Name
                 };
-                var odataModel = this.getView().getModel("nwModel");
+                var odataModel = this.getOwnerComponent().getModel();
                 odataModel.create("/Categories", data, {
-                    headers:{
-                        "Content-ID":"1"
+                    headers: {
+                        "Content-ID": "1"
                     },
-                    success: function(data, response){
+                    success: function (data, response) {
                         MessageBox.success("Data successfully created");
                     },
-                    error: function(error){
+                    error: function (error) {
                         MessageBox.error("Error while creating the data");
                     }
                 });
             },
-            deleteData: function(){
+            deleteData: function () {
                 var list = this.getView().byId("list");
                 var selItem = list.getSelectedItem();
                 var title = selItem.getTitle();
                 var path = "/Categories(" + title + ")"; ///Categories(3);
-                var odataModel = this.getView().getModel("nwModel");
-                odataModel.remove(path,{
-                    headers:{
-                        "Content-ID":"1"
+                var odataModel = this.getOwnerComponent().getModel();
+                odataModel.remove(path, {
+                    headers: {
+                        "Content-ID": "1"
                     },
-                    success: function(data,response){
+                    success: function (data, response) {
                         MessageBox.success("Deleted data");
                     },
-                    error: function(error){
+                    error: function (error) {
                         MessageBox.error("Deletion failed");
                     }
                 })
             },
-            onOpenDialog : function () {
+            onOpenDialog: function () {
 
                 // create dialog lazily
                 if (!this.pDialog) {
                     // this.pDialog = this.loadFragment({
                     //     name: "nw.sapui5democrud.view.fragments.HelloDialog"
                     // });
-                    this.pDialog = sap.ui.xmlfragment("hello","nw.sapui5democrud.view.fragments.HelloDialog", this);
+                    this.pDialog = sap.ui.xmlfragment("hello", "nw.sapui5democrud.view.fragments.HelloDialog", this);
                     this.getView().addDependent(this.pDialog);
-                } 
+                }
                 this.pDialog.open();
                 // this.pDialog.then(function(oDialog) {
                 //     oDialog.open();
                 // });
             },
-            onCloseDialog : function () {
+            onCloseDialog: function () {
                 // note: We don't need to chain to the pDialog promise, since this event-handler
                 // is only called from within the loaded dialog itself.
-                Fragment.byId("hello","helloDialog").close();
+                Fragment.byId("hello", "helloDialog").close();
             }
         });
     });
